22b0635dac9532cbf8f0a2d1b71286b5614d5aca
EveAchnitz <159180793+EveAchnitz@users.noreply.github.com>
2024-05-07 08:55:09 -0300

    Add files via upload

--- Atividades-if or else (Java)/Atividade3.java
+++ Atividades-if or else (Java)/Atividade3.java
@@ -3,36 +3,24 @@ public class Main {
   public static void main(String[] args) {
       
     Scanner leia = new Scanner(System.in);
-    int nota = 0;
     
-    System.out.print("Qual foi sua nota?");
-    nota = leia.nextInt();
+    System.out.print("Digite um valor: ");
+    int x = leia.nextInt();
     
-    if(nota >= 90 && nota <= 100){
-    System.out.print("Parabéns! Sua classificação é A.");
+    System.out.print("Digite outro valor: ");
+    int y = leia.nextInt();
+    
+    if(x != y){
+    if(x > y){
+    System.out.println(x + " é o maior valor.");
     }
-    
-    else if(nota >= 80 && nota <= 89){
-    System.out.print("Quase lá! Sua classificação é B.");
-    }
-    
-    else if(nota >= 70 && nota <= 79){
-    System.out.print("Vamos melhorar! Sua classificação é C.");
-    }
-    
-    else if(nota >= 60 && nota <= 69){
-    System.out.print("Você pode mais! Sua classificação é D.");
-    }
-    
-    else if(nota >= 0 && nota <= 59){
-    System.out.print("Se esforce! Sua classificação é F.");
-    }
-    
     else{
-    System.out.print("Valor inválido.");
+    System.out.println(y + " é o maior valor.");
     }
-
+    }
+    else{
+    System.out.println("Os valores são iguais.");
+    }
     
   }
 }
-
